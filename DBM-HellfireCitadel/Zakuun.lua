9964bede3440e366300ed93bb5eecefb8dcf4aa8
Adam <MysticalOS@users.noreply.github.com>
2017-08-27 05:25:32 +0000

    zakuun inline tweak

--- DBM-HellfireCitadel/Zakuun.lua
+++ DBM-HellfireCitadel/Zakuun.lua
@@ -48,7 +48,7 @@ local yellSeedsofDestruction			= mod:NewYell(181508)
 --Armed
 local timerRumblingFissureCD			= mod:NewNextTimer(39, 179582, 161600, nil, nil, 5)
 local timerBefouledCD					= mod:NewNextTimer(38, 179711, nil, nil, nil, 3, nil, DBM_CORE_HEALER_ICON)
-local timerSoulCleaveCD					= mod:NewNextTimer(40, 179406, nil, nil, nil, 3)
+local timerSoulCleaveCD					= mod:NewNextTimer(40, 179406, nil, nil, nil, 5, nil, DBM_CORE_TANK_ICON)
 local timerCavitationCD					= mod:NewNextTimer(40, 181461, nil, nil, nil, 2)
 --Disarmed
 local timerDisarmCD						= mod:NewNextTimer(85.8, 179667, nil, nil, nil, 6)
